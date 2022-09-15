f27c6a794ee03d1b7914a2cce7166caf7449ae80
ZSYblah <zsyblah@gmail.com>
2022-09-15 13:46:45 +0200

    Feature: Multiple models visualization
    Enables multiple models selected and shown. Need to give a sizable window to show the scene and a movable camera so we can see the details of each mesh.

--- main.py
+++ main.py
@@ -1,14 +1,64 @@
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
-import tkinter as tk
+from trimesh import geometry
+from trimesh import scene
+from trimesh import viewer
+
+from trimesh.scene import lighting
+
+import numpy as np
+import tkinter
 import trimesh
 
+window_size = np.array([1280, 720])
 
-root = tk.Tk()
-root.withdraw()
+light = lighting.DirectionalLight(color=[255, 255, 255, 1], intensity=0, radius=1000)
 
-Filepath = filedialog.askopenfilename(filetypes=[("Mesh", ("*.off", ".ply"))], initialdir=r"./")
+camera = scene.Camera(resolution=window_size, fov=[2.0, 2.0])
 
-mesh = trimesh.load_mesh(Filepath)
+Scene = trimesh.Scene()
 
-mesh.show()
+def test():
+    messagebox.showinfo("Test", "Hello World!")
+
+def selectModel():
+    filePaths = filedialog.askopenfilenames(filetypes=[("Mesh", ("*.off", ".ply"))], initialdir=r"./")
+
+    selectedNumber = len(filePaths)
+
+    if selectedNumber == 0:
+        return
+
+    for i in range(selectedNumber):
+        mesh = trimesh.load_mesh(filePaths[i])
+        mesh.apply_translation([i-selectedNumber/2, 0, 0])
+        Scene.add_geometry(mesh)
+
+    Scene.show()
+    
+
+window = tkinter.Tk()
+
+window.title("Multimedia Retrieval")
+
+window.geometry("960x540")
+
+button_showModel = tkinter.Button(
+    master=window, 
+    text="Select models", 
+    command=selectModel, 
+    height=2, 
+    width=15)
+
+button_showModel.grid(column=0, row=0, padx=10, pady=10)
+
+button_test = tkinter.Button(
+    master=window, 
+    text="Test", 
+    command=test, 
+    height=2, 
+    width=15)
+
+button_test.grid(column=0, row=1, padx=10, pady=10)
+
+window.mainloop()
