d1802d7ee8243763ae162d5fc008c9a669016dab
loahpotato <964040805@qq.com>
2022-09-30 20:32:21 +0200

    update getting bounding_box

---
+++ Filters/boundingBox.py
@@ -0,0 +1,22 @@
+from openpyxl import load_workbook
+import pymeshlab
+
+
+wb = load_workbook('../filter.xlsx')
+ws = wb['Sheet1']
+ws.cell(1, ws.max_column+1).value = 'Bounding Box x'
+ws.cell(1, ws.max_column+1).value = 'Bounding Box y'
+ws.cell(1, ws.max_column+1).value = 'Bounding Box z'
+
+ms = pymeshlab.MeshSet()
+
+for row in range(2, ws.max_row+1):
+    file = ws.cell(row, 1).value
+    label = ws.cell(row, 2).value
+    ms.load_new_mesh("../LabeledDB_new/" + label + "/" + file)
+    ws.cell(row, 6).value = ms.current_mesh().bounding_box().dim_x()
+    ws.cell(row, 7).value = ms.current_mesh().bounding_box().dim_y()
+    ws.cell(row, 8).value = ms.current_mesh().bounding_box().dim_z()
+    ms.clear()
+
+wb.save('../filter.xlsx')
--- Filters/filter.py
+++ Filters/filter.py
@@ -5,7 +5,7 @@ import convertor
 
 # Workbook() takes one, non-optional, argument
 # which is the filename that we want to create.
-workbook = xlsxwriter.Workbook('filter.xlsx')
+workbook = xlsxwriter.Workbook('../filter.xlsx')
 
 # The workbook object is then used to add new
 # worksheet via the add_worksheet() method.
@@ -18,15 +18,15 @@ worksheet.write('B1', 'Class')
 worksheet.write('C1', 'Vertices')
 worksheet.write('D1', 'Faces')
 worksheet.write('E1', 'Type of Faces')
-worksheet.write('F1', 'Bounding box')
 
-root_dir = './LabeledDB_new'
+
+root_dir = '../LabeledDB_new'
 row = 1
 for sub_fold in os.listdir(root_dir):
     d = os.path.join(root_dir, sub_fold)
 
     for file in os.listdir(d):
-        
+
         if file.endswith('.off') or file.endswith('.ply'):
             worksheet.write(row, 0, file)
             worksheet.write(row, 1, sub_fold)
@@ -38,13 +38,6 @@ for sub_fold in os.listdir(root_dir):
             tri = 0
             quad = 0
 
-            maxX = 0
-            minX = 0
-            maxY = 0 
-            minY = 0
-            maxZ = 0
-            minZ = 0
-
             fp = open(file_path)
             next(fp)  # skip first line
             for i, line in enumerate(fp):
@@ -53,29 +46,10 @@ for sub_fold in os.listdir(root_dir):
                     f = int(line.split(' ')[1])
                     worksheet.write(row, 2, v)
                     worksheet.write(row, 3, f)
-
-                elif v > 0:
-                    v -= v  # read lines of vertices
-                    curX = float(line.split(' ')[0])
-                    curY = float(line.split(' ')[1])
-                    curZ = float(line.split(' ')[2])
-
-                    if curX < minX:
-                        minX = curX
-                    else:
-                        maxX = curX
-                    
-                    if curY < minY:
-                        minY = curY
-                    else:
-                        maxY = curY
-
-                    if curZ < minZ:
-                        minZ = curZ
-                    else:
-                        maxZ = curZ
-
                     continue
+
+                if v > 0:
+                    v -= v  # lines of vertices
 
                 else:  # read lines of faces
                     if tri == 1 and quad == 1:
@@ -89,12 +63,6 @@ for sub_fold in os.listdir(root_dir):
                         worksheet.write(row, 4, 'Tri')
                     elif tri == 0:
                         worksheet.write(row, 4, 'Quad')
-                
-                length = maxX - minX
-                width = maxY - minY
-                height = maxZ - minZ
-                bBoxSize = str(length) + ' ' + str(width) + ' ' + str(height)
-                worksheet.write(row, 5, bBoxSize)
 
             row += 1
             print(file)
