6e96d4450ddf32a22e3f2703f7a3f011b8767724
osanmaoo <zuchizheng@gmail.com>
2022-10-04 20:55:10 -0230

    Align to Z-axis

--- PCA.py
+++ PCA.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pymeshlab
-
+'''
 ms = pymeshlab.MeshSet()
 ms.load_new_mesh("./LabeledDB_new/Ant/82.off")
 
 cur_ms = ms.current_mesh()
-
+'''
 
 # Parameters: 
 #   cur_ms: mesh
 #   n: number of remaining dimensions 
-# Return: [Feature valuse, feature vectors, lower dimensions matrix, rebuilded mesh's matrix]
+# Return: [feature vectors(this is an array with descending sorted feature vectors) , lower dimensions matrix, new mesh's matrix]
 def pca(cur_ms, n):
     mesh_np = cur_ms.vertex_matrix()       # get mesh matrix
     mean_val = np.mean(mesh_np, axis=0)    # get mean of mesh's matrix
@@ -40,6 +40,29 @@ def pca(cur_ms, n):
     number_feature_val_index = feature_val_index[-1:-(n+1):-1]
     number_feature_vect = feature_vect[:,number_feature_val_index]
     lowD_martix = new_mesh_np * number_feature_vect
-    rebuild_matrix = (lowD_martix * number_feature_vect.T) + mean_val
+    #rebuild_matrix = (lowD_martix * number_feature_vect.T) + mean_val
 
-    return [feature_val, feature_vect, lowD_martix, rebuild_matrix]
+    return [number_feature_vect, lowD_martix, new_mesh_np]
+
+# calculate the rotation matrix by feacture vector and z-axis vector
+def find_rotation_matrix(number_feature_vect):
+    a = np.array([0, 0, 1])
+    b = number_feature_vect[0]
+    b = np.squeeze(np.asarray(b))
+    v = np.cross(a, b)
+    s = np.linalg.norm(v)
+    c = np.dot(a, b) 
+    vx = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]]) 
+    r = np.eye(3) + vx + np.dot(vx, vx) * (1-c)/(s**2) 
+    return r
+
+# calculate the new mesh matrix after rotation_matri
+# Parameters: mesh matrix, feature vector.
+def align_to_z (new_mesh_np, number_feature_vect):
+    rotation_matrix = find_rotation_matrix(number_feature_vect)
+    print(np.shape(rotation_matrix))
+    print(np.shape(new_mesh_np))
+    new_mesh_np = new_mesh_np.dot(rotation_matrix)
+
+    return new_mesh_np
+
--- pyMeshLabTest.py
+++ pyMeshLabTest.py
@@ -4,6 +4,7 @@ import os
 import polyscope as ps
 
 import create_bbox
+import PCA
 
 # Polyscope initialized
 ps.init()
@@ -14,7 +15,7 @@ ms = pymeshlab.MeshSet()
 # load a new mesh in the MeshSet, and sets it as current mesh
 # the path of the mesh can be absolute or relative
 
-ms.load_new_mesh("./LabeledDB_new/Ant/82.off")
+ms.load_new_mesh("./LabeledDB_new/Airplane/61.off")
 
 
 #display the bounding box of current mesh
@@ -23,10 +24,18 @@ dim_y = ms.current_mesh().bounding_box().dim_y()
 dim_z = ms.current_mesh().bounding_box().dim_z()
 bbox_max = ms.current_mesh().bounding_box().max()
 bbox_min = ms.current_mesh().bounding_box().min()
+
+mesh_face_matrix = ms.current_mesh().face_matrix()
+mt = ms.current_mesh().vertex_matrix()
 #create a off file to save the dounding box
 create_bbox.creat_bounding_box_file(bbox_max, bbox_min, dim_x, dim_y, dim_z)
 create_bbox.draw_bounding_box(bbox_min, bbox_max, dim_x, dim_y, dim_z)
 
+PCA_value = PCA.pca(ms.current_mesh(), 3)
+
+new_mesh = PCA.align_to_z(PCA_value[2], PCA_value[0])
+
+ps.register_surface_mesh('new', new_mesh, mesh_face_matrix)
 
 '''
 print(ms.current_mesh().face_number())
